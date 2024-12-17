604cd731ce494bfdf55649a07975bac42f578f67
casper <casper.adriaensen@gmail.com>
2024-12-17 16:43:51 +0100

    fix the getAllPosts end point

--- front-end/service/PostService.ts
+++ front-end/service/PostService.ts
@@ -1,11 +1,9 @@
 import { Post } from "@types";
 
 const getAllPosts = async () => {
-    const token = localStorage.getItem('token');
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/post/`, {
         method: 'GET',
         headers: {
-            Authorization: `Bearer ${token}`
         }
     })
 
